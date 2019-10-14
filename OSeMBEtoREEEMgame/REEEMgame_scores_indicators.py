e2b2bd9a66718c5d64efe99cafea0c9317e29b50
Hauke Henke <haukeh@UG.KTH.SE>
2019-10-14 18:48:20 +0200

    Completed function to import population data from EC, 2016 Reference Scenario excel and NEWAGE excel.

--- OSeMBEtoREEEMgame/REEEMgame_scores_indicators.py
+++ OSeMBEtoREEEMgame/REEEMgame_scores_indicators.py
@@ -6,56 +6,93 @@ Created on Mon Oct  7 08:09:14 2019
 """
 
 import pandas as pd
-
-#%% Import function to import all data needed, from REEEMdb and from excel with populations forecast
-def ImportData():
+import numpy as np
+#%% Import function to import data from REEEMdb
+def import_reeemdb():
     rawData = pd.DataFrame()
     return rawData
+#%% Import from excel file
+def import_excel(file_name, countries):
+    pop_dic = {}
+    non_list_countr = ['CH','NO']
+    for country in countries:
+        pop_dic[country] = pd.DataFrame()
+        pop_data = pd.DataFrame(index=pd.Series(range(2015,2051)))
+        pop_data['population'] = np.nan
+        if country == 'CH' or country == 'NO':
+            raw_data = pd.read_excel('pop_projection_NEWAGE_CH_NO.xlsx','MaGe Factors',usecols="AN:BW",nrows=2)
+            raw_data.index = non_list_countr
+            raw_data = raw_data.transpose()
+            raw_data.index = pd.Series(range(2015,2051))
+            raw_data = raw_data.multiply(1000)
+            pop_data['population'] = raw_data[country]
+            pop_dic[country] = pop_data
+        else:
+            sheet = str(country+'-A')
+            years = pd.read_excel(file_name,sheet,usecols="E:L",nrows=1)
+            years = years.iloc[0]
+            years = years.tolist()
+            pop = pd.read_excel(file_name,sheet,usecols="E:L",skiprows=2,nrows=1)
+            pop = pop.transpose()
+            pop.index = years
+            for y in years:
+                pop_data.loc[y]['population'] = pop.loc[y]
+            pop_data = pop_data.interpolate()
+            pop_data = pop_data.multiply(1000000)
+            pop_dic[country] = pop_data
+    return pop_dic
 #%% Calculation of CO2 intensity per citizen
-def CO2Intensity():
+def co2intensity():
     CO2Intensity = pd.DataFrame()
     return CO2Intensity
 #%% Calculation of the Discounted Investment per Citizen
-def DiscInvestment():
-    DiscInvestment = pd.DataFrame()
-    return DiscInvestment
+def disc_investment():
+    Disc_Investment = pd.DataFrame()
+    return Disc_Investment
 #%% Calculation of Capital Recovery Factor
-def CRF():
-    CRF = pd.DataFrame()
-    return CRF
+def crf():
+    crf = pd.DataFrame()
+    return crf
 #%% Calculation of the Capital Investment per country, technology and year
-def CI():
-    CI =pd.DataFrame()
-    return CI
+def ci():
+    ci =pd.DataFrame()
+    return ci
 #%% Calculation of the Annualized Investment Cost
-def AIC():
-    AIC = pd.DataFrame()
-    return AIC
+def aic():
+    aic = pd.DataFrame()
+    return aic
 #%% Calculation of the Annual Fixed Operating Cost
-def AFOC():
+def afoc():
     AFOC = pd.DataFrame()
     return AFOC
 #%% Calculation of the Annual Variable Operating Cost
-def AVOC():
+def avoc():
     AVOC = pd.DataFrame()
     return AVOC
 #%% Calculation of the Dpmesstic Electricity Production per country
-def DEP():
+def dep():
     DEP = pd.DataFrame()
     return DEP
 #%% Calculation of the Levelised Cost of Domestic Electricity
-def LCoDE():
+def lcode():
     LCoDE = pd.DataFrame
     return LCoDE
 #%% Calculation of the countryspecific LCOEs
-def LCOE():
+def lcoe():
     LCOE = pd.DataFrame()
     return LCOE
 #%% Calculation of the Share of national electricity demand in the European electricity Demand
-def DemandShare():
+def demand_share():
     DemandShare = pd.DataFrame()
     return DemandShare
 #%% Calculation of the EULCOE
-def EULCOE():
+def eu_lcoe():
     EULCOE = pd.DataFrame()
     return EULCOE
+#%% Main function
+def main():
+    xls = 'EC, 2016 - AppendixRefSce.xls'
+    countries = ['AT','BE','BG','CH','CY','CZ','DE','DK','EE','ES','FR','FI','GR','HR','HU','IE','IT','LT','LU','LV','MT','NL','NO','PL','PT','RO','SE','SI','SK','UK']
+    pop_raw = import_excel(xls,countries)
+    return pop_raw
+output = main()
