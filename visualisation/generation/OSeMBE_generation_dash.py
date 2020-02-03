d041cef6b063ea9791005fa0b7001827a21e519d
HauHe <haukeh@kth.se>
2020-02-03 16:15:56 +0100

    Now showing two graphs each with dropdown menues for selecting country and pathway, next to each other. Still pending is to have a fix order of fuels.

--- visualisation/generation/OSeMBE_generation_dash.py
+++ visualisation/generation/OSeMBE_generation_dash.py
@@ -35,33 +35,96 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 app.layout = html.Div(children=[
-    html.H1(children='Power generation'),
-    
-    html.Label('Pathway'),
-    dcc.Dropdown(
-        id='pathway-selection',
-        options = [{'label': i, 'value': i} for i in pathways],
-        value = 'B1C0T0E0'
-        ),
-    
-    dcc.Dropdown(
-        id='region-country-selection',
-        options = [{'label': i, 'value': i} for i in regions],
-        value = 'EU+CH+NO'
-        ),
-    
-    dcc.Graph(
-        id='Power-generation'
-        )
+    html.H1(children='OSeMBE results'),
+    html.H2(children='Power generation'),
+    html.Div([        
+        html.Label('Pathway 1'),
+        dcc.Dropdown(
+            id='pathway-selection-1',
+            options = [{'label': i, 'value': i} for i in pathways],
+            value = 'B1C0T0E0'
+            ),
+        html.Label('Region/Country 1'),
+        dcc.Dropdown(
+            id='region-country-selection-1',
+            options = [{'label': i, 'value': i} for i in regions],
+            value = 'EU+CH+NO'
+            ),
+        
+        dcc.Graph(
+            id='Power-generation-1'
+            )
+        ], style={'width': '49%', 'display': 'inline-block'}),
+    html.Div([
+        html.Label('Pathway 2'),
+        dcc.Dropdown(
+            id='pathway-selection-2',
+            options = [{'label': i, 'value': i} for i in pathways],
+            value = 'B1C0T0E0'
+            ),
+        html.Label('Region/Country 2'),
+        dcc.Dropdown(
+            id='region-country-selection-2',
+            options = [{'label': i, 'value': i} for i in regions],
+            value = 'EU+CH+NO'
+            ),
+        
+        dcc.Graph(
+            id='Power-generation-2'
+            )
+        ],style = {'width': '49%', 'display': 'inline-block', 'float': 'right'})
     ])
+# app.css.append_css({
+#     'external_url': 'https://codepen.io/chriddyp/pen/bWLwgP.css'
+# })
 
 @app.callback(
-    Output('Power-generation', 'figure'),
-    [Input('pathway-selection', 'value'),
-     Input('region-country-selection', 'value')])
+    Output('Power-generation-1', 'figure'),
+    [Input('pathway-selection-1', 'value'),
+     Input('region-country-selection-1', 'value')])
 
 #%% Function for updating graph
-def update_graph(selected_pathway, selected_region):
+def update_graph_1(selected_pathway, selected_region):
+    filtered_df = df[(df['pathway'] == selected_pathway) & (df['region'] == selected_region)]
+    filtered_df_p = filtered_df.pivot(index='year', columns='indicator',  values='value')
+    years = filtered_df['year'].unique()
+    traces = []
+    fuel_abre = pd.DataFrame({'fuel_name':['Wind','Hydro','Biofuel liquid','Coal','Biomass solid','Waste non renewable','Oil','Nuclear','Natural gas / non renew.','Ocean','Geothermal','Solar'],'fuel_abr':['wind','hydro','biofuel','coal','biomass','waste','oil','nuclear','gas','ocean','geo','solar']}, columns = ['fuel_name','fuel_abr'])
+    #%% Facts dict
+    info_dict = {}
+    info_dict['Filename'] = ['{}_OSeMBE_plot_generation' .format(pd.to_datetime('today').strftime("%Y-%m-%d"))]
+    info_dict['Unit'] = filtered_df.loc[:,'unit'].unique()
+    info_dict['Pathway'] = filtered_df.loc[:,'pathway'].unique()
+    info_dict['Year'] = filtered_df.loc[:,'year'].unique().tolist()
+    info_dict['Y-Axis'] = ['{}'.format(*info_dict['Unit'])]
+    for i in filtered_df['indicator'].unique():
+        temp = fuel_abre.loc[fuel_abre['fuel_name']==i,'fuel_abr']
+        fuel_code = temp.iloc[0]
+        traces.append(dict(
+            x = years,
+            y = filtered_df_p.loc[:,i],
+            hoverinfo='x+y',
+            mode='lines',
+            line=dict(width=0.5,
+                      color=colours[fuel_code]),
+            stackgroup='one',
+            name=i
+            ))
+    return {
+        'data': traces,
+        'layout': dict(
+            title='Electricity generation in {} in scenario {}'.format(selected_region,selected_pathway),
+            yaxis=dict(title=''.join(info_dict['Y-Axis'])),
+            font=dict(family='Aleo'),
+            )
+        }
+
+@app.callback(
+     Output('Power-generation-2', 'figure'),
+    [Input('pathway-selection-2', 'value'),
+     Input('region-country-selection-2', 'value')])
+#%% Function for updating graph
+def update_graph_2(selected_pathway, selected_region):
     filtered_df = df[(df['pathway'] == selected_pathway) & (df['region'] == selected_region)]
     filtered_df_p = filtered_df.pivot(index='year', columns='indicator',  values='value')
     years = filtered_df['year'].unique()
