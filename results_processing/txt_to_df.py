ebcc64ab590df916249bee2ad4c7e89fbbbe49c7
HauHe <haukeh@kth.se>
2020-02-06 12:17:33 +0100

    Section on metadata writte and read in of txt results file.

--- results_processing/txt_to_df.py
+++ results_processing/txt_to_df.py
@@ -10,3 +10,18 @@ import numpy as np
 import os
 
 sol_txts = []
+for root, dirs, files in os.walk("."):
+    for file in files:
+        if file.endswith('.txt'):
+            sol_txts.append(file)
+
+for file in sol_txts:
+    raw_data = file.split('_')
+    metadata = {}
+    metadata['model'] = raw_data[0]
+    metadata['pathway'] = raw_data[3]
+    metadata['date'] = '2019-07-29'
+    metadata['data-version'] = 'DataV2'
+    metadata['input-output'] = 'Output'
+    
+    data = pd.read_csv(file, names=['Year'])
