318c11f3f05177af49f547d095d9eac6ddc324de
HauHe <haukeh@kth.se>
2020-02-07 15:52:15 +0100

    Converted initial sections of script into functions. Started work on building sorted dfs for each results parameter.

--- results_processing/txt_to_df.py
+++ results_processing/txt_to_df.py
@@ -9,13 +9,19 @@ import pandas as pd
 import numpy as np
 import os
 
-sol_txts = []
-for root, dirs, files in os.walk("."):
-    for file in files:
-        if file.endswith('.txt'):
-            sol_txts.append(file)
+def get_file_names():
+    sol_txts = []
+    for root, dirs, files in os.walk("."):
+        for file in files:
+            if file.endswith('.txt'):
+                sol_txts.append(file)
+    return sol_txts
 
-for file in sol_txts:
+def get_results(file):
+    data = pd.read_csv(file, names=['Year'])
+    return data
+
+def metadata_dic(file):
     raw_data = file.split('_')
     metadata = {}
     metadata['model'] = raw_data[0]
@@ -23,5 +29,22 @@ for file in sol_txts:
     metadata['date'] = '2019-07-29'
     metadata['data-version'] = 'DataV2'
     metadata['input-output'] = 'Output'
-    
-    data = pd.read_csv(file, names=['Year'])
+    return metadata
+
+def results_to_dfs(metadata, data):
+    results_dic = {}
+    data = pd.DataFrame(data.Year.str.split('\t', 2).tolist(), columns = ['Parameter', 'Region', 'rest'])
+    data = data.drop(['Region'], axis=1)
+    parameters = data['Parameter'].unique()
+    for parameter in parameters:
+        results_dic[parameter] = data[data['Parameter']==parameter]
+        df = results_dic[parameter]
+        df = df['rest'].str.split('\t', expand=True)
+        results_dic[parameter] = df
+    return results_dic
+
+sol_txts = get_file_names()
+
+for file in sol_txts:
+    metadata = metadata_dic(file)
+    data = get_results(file)
