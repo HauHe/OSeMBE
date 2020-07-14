4f1fec044e223c7794f98db9c85045119cd24a67
Hauke Henke <haukeh@UG.KTH.SE>
2020-07-14 17:52:50 +0200

    Added function to create matrix of exchanged electricity for a selected path and year.

--- visualisation/trade/trade_matrix.py
+++ visualisation/trade/trade_matrix.py
@@ -32,7 +32,31 @@ def get_raw_df(file):
             |(df['year']=='2040')
             |(df['year']=='2050')]
     df['country'] = df['info_1'].apply(lambda x: x[:2])
+    df['fuel_countr'] = df['info_2'].apply(lambda x: x[:2])
+    df['fuel_fuel'] = df['info_2'].apply(lambda x: x[2:])
+    df = df[df['fuel_fuel']=='E1']
     return df
+
+def build_matrix(df, path, year):
+    # df = df_raw #for development, hashout after  or delete
+    # path = path_sel #for development, hashout after  or delete
+    # year = year_sel #for development, hashout after  or delete
+    
+    df = df[(df['year']==year)&(df['pathway']==path)]
+    countries = df['fuel_countr'].unique()
+    matrix = pd.DataFrame(0, index=countries, columns=countries)
+    countries_a = df['country'].unique()
+    for country in countries_a:
+        # country = 'AT' #for development, hashout after  or delete
+        df_countr = df[df['country']==country]
+        countr_con = df_countr['tech_countr'].unique()
+        for con in countr_con:
+            # con = 'CZ' #for development, hashout after  or delete
+            atob = df_countr['value'][(df_countr['tech_countr']==con)&(df_countr['fuel_countr']==con)]
+            matrix.loc[country,con] = atob.iloc[0]
+            btoa = df_countr['value'][(df_countr['tech_countr']==con)&(df_countr['fuel_countr']==country)]
+            matrix.loc[con,country] = btoa.iloc[0]
+    return matrix
 #%% main
 
 pkl_list = get_file_names()
@@ -43,3 +67,11 @@ if len(pkl_list)>1:
 else:
     pkl_file = pkl_list[0]
 df_raw = get_raw_df(pkl_file)
+for path in df_raw['pathway'].unique():
+    print(path)
+path_sel = input('Please select one of the above listed scenarios by typing its name: ')
+for year in df_raw['year'].unique():
+    print(year)
+year_sel= input('Please select one of the above listed years to generate the electricitr exchange matrix for:')
+
+exchange_matrix = build_matrix(df_raw, path_sel, year_sel)
